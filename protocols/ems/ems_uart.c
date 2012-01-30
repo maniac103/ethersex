f67ba17de0183d530a39f5f30ad766fff3205c44
Danny Baumann <dannybaumann@web.de>
2012-01-30 10:10:40 +0100

    Remove untested and now-broken soft UART code.

--- protocols/ems/ems_uart.c
+++ protocols/ems/ems_uart.c
@@ -32,9 +32,23 @@
 #define PRESCALE 8
 #define BIT_TIME  ((uint8_t)((F_CPU / BAUD) / PRESCALE))
 
+#define USE_USART 1
+#include "core/usart.h"
+
 static volatile uint8_t state = STATE_RX;
 static volatile uint8_t bit_counter = 0;
 static volatile uint8_t sent_data = 0;
+
+/* We generate our own usart init module, for our usart port */
+generate_usart_init()
+
+void
+ems_uart_init(void)
+{
+  usart_init();
+  TC2_PRESCALER_8;
+  TC2_MODE_CTC;
+}
 
 static inline uint8_t is_polled(void)
 {
@@ -50,20 +64,6 @@ static uint8_t get_next_tx_byte(uint8_t *byte)
     return 1;
   }
   return 0;
-}
-
-#if !defined(EMS_SOFT_UART)
-#define USE_USART 1
-#include "core/usart.h"
-/* We generate our own usart init module, for our usart port */
-generate_usart_init()
-
-void
-ems_uart_init(void)
-{
-  usart_init();
-  TC2_PRESCALER_8;
-  TC2_MODE_CTC;
 }
 
 static inline void
@@ -178,137 +178,4 @@ ISR(usart(USART,_RX_vect))
       break;
   }
 }
-
-#else /* !EMS_SOFT_UART */
-
-#define PRESCALE 8
-#define BIT_TIME  ((uint8_t)((F_CPU / BAUD) / PRESCALE))
-
-static volatile uint8_t current_mask;
-static volatile uint8_t current_data;
-static volatile uint8_t send_counter = 0;
-
-static void set_rx_int(uint8_t enable)
-{
-  if (enable) {
-    GIFR = _BV(EMS_SOFTRX_INT_PIN);
-    _EIMSK |= _BV(EMS_SOFTRX_INT_PIN);
-  } else {
-    _EIMSK &= ~_BV(EMS_SOFTRX_INT_PIN);
-  }
-}
-void
-ems_uart_init(void)
-{
-  DDR_CONFIG_IN(EMS_UART_RX);
-  DDR_CONFIG_OUT(EMS_UART_TX);
-  PIN_SET(EMS_UART_TX);
-
-  TC2_COUNTER_CURRENT = 0;
-  TC2_PRESCALER_8;
-
-  /* Initialize Interrupt */
-  _EICRA = (uint8_t) ((_EICRA & ~EMS_SOFTRX_INT_ISCMASK) | EMS_SOFTRX_INT_ISC);
-  set_rx_int(1);
-}
-
-ISR(EMS_SOFTRX_INT_VECTOR)
-{
-  TC2_COUNTER_COMPARE = TC2_COUNTER_CURRENT + (uint8_t)((BIT_TIME * 3) / 2);// scan 1.5 bits after start
-
-  current_mask = 1;
-  current_data = 0;
-
-  if (!PIN_HIGH(EMS_UART_RX)) {
-    set_rx_int(0);
-    TC2_INT_COMPARE_ON;
-  }
-}
-
-static void
-init_tx(uint8_t byte)
-{
-  current_data = byte;
-  send_counter = 9;
-  TC2_OUTPUT_COMPARE_SET;
-}
-
-static void
-check_tx_or_go_to_rx(void)
-{
-  switch (state) {
-    case STATE_RX:
-      if (is_polled()) {
-        ems_set_led(LED_BLUE, 1, 0);
-        state = STATE_TX_ADDR;
-        init_tx(OUR_EMS_ADDRESS);
-      }
-      break;
-    case STATE_TX_ADDR:
-      {
-        uint8_t byte;
-        if (get_next_tx_byte(&byte)) {
-          init_tx(byte);
-        } else {
-          state = STATE_TX_BREAK;
-          init_tx(0);
-        }
-      }
-      break;
-    case STATE_TX_BREAK:
-    default:
-      ems_set_led(LED_BLUE, 1, 10);
-      state = STATE_RX;
-      TC2_OUTPUT_COMPARE_NONE;
-      TC2_INT_COMPARE_OFF;
-      set_rx_int(1);
-      break;
-  }
-}
-
-ISR(TC2_VECTOR_COMPARE)
-{
-  uint8_t in = PIN_HIGH(EMS_UART_RX);
-  TC2_COUNTER_COMPARE += BIT_TIME;
-
-  if (state == STATE_RX) {
-    if (current_mask) {
-      if (in) {
-        current_data |= current_mask;
-      }
-      current_mask <<= 1;
-    } else {
-      uint8_t status = in ? 0 : FRAMEEND;
-      ems_uart_process_input_byte(current_data, status);
-      check_tx_or_go_to_rx();
-    }
-  } else /* transmit */ {
-    if (send_counter == 0) {
-      check_tx_or_go_to_rx();
-    } else {
-      send_counter--;
-
-      uint8_t lastbit = send_counter == 8 ? 0 : (current_data & _BV(send_counter));
-      uint8_t nextbit;
-
-      if (send_counter == 0) {
-        nextbit = state == STATE_TX_BREAK ? 0 : 1;
-      } else {
-        nextbit = (current_data & _BV(send_counter - 1));
-      }
-
-      if ((lastbit && !in) || (!lastbit && in)) {
-        /* XXX: abort */
-      }
-
-      if (nextbit) {
-        TC2_OUTPUT_COMPARE_SET;
-      } else {
-        TC2_OUTPUT_COMPARE_CLEAR;
-      }
-    }
-  }
-}
-
-#endif /* !EMS_SOFT_UART */
 
